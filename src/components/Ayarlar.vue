d0dcda879c3788b707984ae51a25d5134e36f661
Abdullah Tunçer <abdullah.1755.1@gmail.com>
2025-07-03 10:28:19 +0300

    v2.0.1

--- src/components/Ayarlar.vue
+++ src/components/Ayarlar.vue
@@ -10,6 +10,8 @@
         <anasayfa-ayarlari/>
         <v-divider/>
         <urun-ayarlari/>
+        <v-divider/>
+        <p @click="versiyonSayfasi" align="center" class="text-subtitle-1 my-1 text-grey">{{ version }}</p>
       </v-card>
     </v-col>
   </v-row>
@@ -19,4 +21,11 @@
 import SistemAyarlari from "./SistemAyarlari.vue";
 import AnasayfaAyarlari from "./AnasayfaAyarlari.vue";
 import UrunAyarlari from "./UrunAyarlari.vue";
+import {Browser} from '@capacitor/browser';
+
+const version = ("v" + __APP_VERSION__)
+
+const versiyonSayfasi = async () => {
+  await Browser.open({url: 'https://github.com/abdullah-tuncer/mobil-vue-inventory-scanner/releases'});
+}
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { version } from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    __APP_VERSION__: JSON.stringify(version),
+  },
   test: {
     globals: true,
     environment: 'jsdom',
